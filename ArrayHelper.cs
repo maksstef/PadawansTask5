2a31ae19fc1eaca17c6cfeb030a13f703771f9f3
mxmstfnk <boss.canta@mail.ru>
2019-02-12 18:35:00 +0300

    1

---
+++ ArrayHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PadawansTask5
+{
+    public static class ArrayHelper
+    {
+        public static string CheckIfSymmetric(int[] source)
+        {
+            // put your code here
+            for (int i = 0; i < source.Length / 2; ++i)
+            {
+                if (source[i] != source[source.Length - 1 - i])
+                {
+                    return "No";
+                }
+            }
+            return "Yes";
+        }
+    }
+}
